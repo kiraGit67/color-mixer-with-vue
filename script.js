dd6cd4382343b9a8c0394d2f50d166a72afa9d42
kiraGit67 <kirsten@sais.de>
2023-05-05 12:36:53 +0200

    adds computed property into data() funktion

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ Vue.createApp({
       blue: 180,
     };
   },
+  computed: {
+    rgb2hex() {
+      let hexValue = "#";
+
+      const hexRed = parseInt(this.red).toString(16);
+      const hexGreen = parseInt(this.green).toString(16);
+      const hexBlue = parseInt(this.blue).toString(16);
+
+      hexValue += hexRed.length === 1 ? "0" + hexRed : hexRed;
+      hexValue += hexGreen.length === 1 ? "0" + hexGreen : hexGreen;
+      hexValue += hexBlue.length === 1 ? "0" + hexBlue : hexBlue;
+
+      return hexValue;
+    },
+  },
   methods: {
     setBackgroundColor(event) {
       console.log(event.target.id);
